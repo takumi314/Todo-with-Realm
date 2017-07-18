741bb9a74b13a5079cce2fc62f0e692baa01d6e9
takumi314 <isospin_symmetry@me.com>
2017-07-19 00:56:33 +0900

    make new UITableViewCell

---
+++ Todo-with-Realm/TodoCellView.swift
@@ -0,0 +1,13 @@
+//
+//  TodoCellView.swift
+//  Todo-with-Realm
+//
+//  Created by NishiokaKohei on 2017/07/19.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+class TodoCellView: UITableViewCell {
+     
+}
