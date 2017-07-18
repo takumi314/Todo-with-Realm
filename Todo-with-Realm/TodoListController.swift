69d5e43bbee22be2cd5936c120874e139e82ab8c
takumi314 <isospin_symmetry@me.com>
2017-07-19 00:31:07 +0900

    make new Controller for main list

---
+++ Todo-with-Realm/TodoListController.swift
@@ -0,0 +1,13 @@
+//
+//  TodoListController.swift
+//  Todo-with-Realm
+//
+//  Created by NishiokaKohei on 2017/07/19.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+class TodoListController: UIViewController {
+    
+}
