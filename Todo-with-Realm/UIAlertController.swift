8b9a150b0328973ac0e63b6b789cae1db69ec28d
takumi314 <isospin_symmetry@me.com>
2017-07-20 08:36:12 +0900

    add extension file

---
+++ Todo-with-Realm/UIAlertController.swift
@@ -0,0 +1,16 @@
+//
+//  UIAlertController.swift
+//  Todo-with-Realm
+//
+//  Created by NishiokaKohei on 2017/07/19.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+extension UIAlertController: UIPickerViewDelegate {
+
+    func addDatePicker(configurationHandler: ((UIDatePicker) -> Swift.Void)? = nil) {
+    }
+
+}
