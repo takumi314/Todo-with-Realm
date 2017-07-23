6cf837b61469bd0af2a50ea0550f9266a9c2b66b
takumi314 <isospin_symmetry@me.com>
2017-07-23 15:58:47 +0900

    make new protocol for Nib file

---
+++ Todo-with-Realm/NibBase.swift
@@ -0,0 +1,12 @@
+//
+//  NibBase.swift
+//  Todo-with-Realm
+//
+//  Created by NishiokaKohei on 2017/07/23.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+protocol Nibable: NSObjectProtocol {
+}
