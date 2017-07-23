8d80f83c8c714ab998ad27288696ea1c4a96a3ad
takumi314 <isospin_symmetry@me.com>
2017-07-24 00:39:15 +0900

    make  a class inherited from UIAlertController for inputing Todo

---
+++ Todo-with-Realm/TodoInputController.swift
@@ -0,0 +1,12 @@
+//
+//  TodoInputController.swift
+//  Todo-with-Realm
+//
+//  Created by NishiokaKohei on 2017/07/24.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+class TodoInputController: UIAlertController {
+}
