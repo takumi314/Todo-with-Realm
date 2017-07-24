e92ae8963443a3a0076fe9debfd36ed10c6adcdc
takumi314 <isospin_symmetry@me.com>
2017-07-24 23:39:33 +0900

    add new view controller for editing Todo

---
+++ Todo-with-Realm/TodoEditController.swift
@@ -0,0 +1,13 @@
+//
+//  TodoEditController.swift
+//  Todo-with-Realm
+//
+//  Created by NishiokaKohei on 2017/07/24.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+class TodoEditController: UIViewController {
+    
+}
