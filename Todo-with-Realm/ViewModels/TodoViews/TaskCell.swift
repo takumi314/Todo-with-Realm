ba174428dcd80b69345b591d1ac03a89f0ecf60f
takumi314 <isospin_symmetry@me.com>
2017-07-25 01:55:58 +0900

    add new cell for task

---
+++ Todo-with-Realm/ViewModels/TodoViews/TaskCell.swift
@@ -0,0 +1,13 @@
+//
+//  TaskCell.swift
+//  Todo-with-Realm
+//
+//  Created by NishiokaKohei on 2017/07/25.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+    
+}
