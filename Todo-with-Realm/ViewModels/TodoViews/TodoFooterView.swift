2e7cb94149ac949dfaa559d16d43e09beba4c4d1
takumi314 <isospin_symmetry@me.com>
2017-07-25 22:55:36 +0900

    make footer view

---
+++ Todo-with-Realm/ViewModels/TodoViews/TodoFooterView.swift
@@ -0,0 +1,17 @@
+//
+//  TodoFooterView.swift
+//  Todo-with-Realm
+//
+//  Created by NishiokaKohei on 2017/07/25.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+protocol TodoFooterViewDelegate {
+    
+}
+
+class TodoFooterView: UITableViewHeaderFooterView {
+
+}
