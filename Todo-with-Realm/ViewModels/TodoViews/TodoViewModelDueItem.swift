a15bf41925d56182aef3ecb1c5af1af6a73d7e4b
takumi314 <isospin_symmetry@me.com>
2017-07-30 18:20:13 +0900

    update views of the due date

--- Todo-with-Realm/ViewModels/TodoViews/TodoViewModelDueItem.swift
+++ Todo-with-Realm/ViewModels/TodoViews/TodoViewModelDueItem.swift
@@ -19,14 +19,25 @@ class TodoViewModelDueItem: TodoViewModelItem {
     }
 
     var sectionTitle: String {
-        return dueDate.description
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd hh:mm"
+        formatter.timeZone = TimeZone.autoupdatingCurrent
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        formatter.locale = Locale.autoupdatingCurrent
+
+        var due = "No Due yet"
+        if let date = dueDate {
+            due = formatter.string(from: date)
+        }
+        return due
     }
 
     var isCollapsed = true
 
-    var dueDate: Date
+    var dueDate: Date?
 
-    init(date: Date) {
+    init(date: Date?) {
         self.dueDate = date
     }
     
