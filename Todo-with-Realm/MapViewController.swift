19288a969d73938086935cf73cce1c811189c947
takumi314 <isospin_symmetry@me.com>
2017-08-02 00:01:55 +0900

    make new view controller for MapView

---
+++ Todo-with-Realm/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  Todo-with-Realm
+//
+//  Created by NishiokaKohei on 2017/08/01.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+}
