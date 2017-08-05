b8ca926aceaf2c8c091fb00cf8aceba2e6964668
takumi314 <isospin_symmetry@me.com>
2017-08-05 18:12:13 +0900

    import CoreLocation

---
+++ Todo-with-Realm/LocationListener.swift
@@ -0,0 +1,14 @@
+//
+//  LocationListener.swift
+//  Todo-with-Realm
+//
+//  Created by NishiokaKohei on 2017/08/05.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationListener {
+
+}
