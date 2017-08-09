bbcb506060d16c51bcab386655c27bc2e5de8900
takumi314 <isospin_symmetry@me.com>
2017-08-10 01:01:31 +0900

    add new cell

---
+++ Todo-with-Realm/ViewModels/TodoViews/LocationCell.swift
@@ -0,0 +1,15 @@
+//
+//  LocationCell.swift
+//  Todo-with-Realm
+//
+//  Created by NishiokaKohei on 2017/08/10.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+}
+
+extension LocationCell: CellIdentifiable {}
