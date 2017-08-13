ce52f9e807904902fef726ce8d2ffb5cc65003d3
takumi314 <isospin_symmetry@me.com>
2017-08-13 23:19:03 +0900

    confirm TodoViewModelItem to this class

--- Todo-with-Realm/ViewModels/TodoViewModelLocationItem.swift
+++ Todo-with-Realm/ViewModels/TodoViewModelLocationItem.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+class TodoViewModelLocationItem: TodoViewModelItem {
+
+    var type: TodoViewModelItemType {
+        get {
+            return .location
+        }
+    }
+    var sectionTitle: String {
+        get {
+            guard let adress = self.address else {
+                return "No address"
+            }
+            return adress
+        }
+    }
+    var isCollapsed: Bool = true
+
+    var address: String?
+
+    init(address: String?) {
+        self.address = address
+    }
+
+}
+
+extension TodoViewModelLocationItem {
+
+}
