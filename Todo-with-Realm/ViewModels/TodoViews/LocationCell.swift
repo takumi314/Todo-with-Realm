626335be9f284af806f39e2879d468c0bcd6833c
takumi314 <isospin_symmetry@me.com>
2017-08-13 23:14:19 +0900

    make new prortocol for Location

--- Todo-with-Realm/ViewModels/TodoViews/LocationCell.swift
+++ Todo-with-Realm/ViewModels/TodoViews/LocationCell.swift
@@ -9,9 +9,39 @@
 import UIKit
 import MapKit
 
+protocol LocationCelldelegate {
+    //
+}
+
 class LocationCell: UITableViewCell {
+
+    // MARK: - IBOutlets
+
     @IBOutlet weak var mapView: MKMapView!
+
+    // MSRK: - Public properties
+
+    var delegate: LocationCelldelegate?
+
+
+    // MARK: - Life cycle
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
 
 }
 
 extension LocationCell: CellIdentifiable {}
+
+extension LocationCell {
+
+    func didTapMap(_ sender: Any) {
+        // デリゲート呼び出し
+    }
+
+}
