c873bb5cfe591a1da9647b524d1f85da59905d39
takumi314 <isospin_symmetry@me.com>
2017-08-13 16:38:17 +0900

    make new HeaderView for Location

--- Todo-with-Realm/ViewModels/TodoHeaderLocationView.swift
+++ Todo-with-Realm/ViewModels/TodoHeaderLocationView.swift
@@ -9,14 +9,39 @@
 import UIKit
 import MapKit
 
+protocol TodoHeaderLocationViewDelegate {
+    //
+}
+
 class TodoHeaderLocationView: UITableViewHeaderFooterView {
-    
+
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var addressLabel: UILabel!
+
+    // MARK: - Public properties
+
+    var item: TodoViewModelItem? {
+        didSet {
+            guard let item = item else { return }
+            self.addressLabel.text = item.sectionTitle
+        }
+    }
+
+    var section: Int = 3
+
+    var delegate: TodoHeaderLocationViewDelegate?
+
+    // MARK: - Life cycle
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
 }
 
 extension TodoHeaderLocationView: HeaderFooterIdentifiable {}
 
 extension TodoHeaderLocationView: TableViewHeaderTouchable {
-    var section: Int {
-        return 3
-    }
+
 }
