fe5eebfbca28cd2c8ae2ef8ae175062b47166a5d
takumi314 <isospin_symmetry@me.com>
2017-08-13 12:46:45 +0900

    add a new TodoViewModelItem file for location

---
+++ Todo-with-Realm/ViewModels/TodoViewModelLocationItem.swift
@@ -0,0 +1,9 @@
+//
+//  TodoViewModelLocationItem.swift
+//  Todo-with-Realm
+//
+//  Created by NishiokaKohei on 2017/08/13.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import Foundation
