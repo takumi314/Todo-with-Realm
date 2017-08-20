74572f5e0dacd6a2999b5241be2ec914365a037f
takumi314 <isospin_symmetry@me.com>
2017-08-20 11:11:50 +0900

    add LocationSearchViewController

---
+++ Todo-with-Realm/LocationSearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LocationSearchViewController.swift
+//  Todo-with-Realm
+//
+//  Created by NishiokaKohei on 2017/08/20.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+final class LocationSearchViewController: UIViewController {
+
+    // MARK: - Life cycle
+
+    
+
+
+}
